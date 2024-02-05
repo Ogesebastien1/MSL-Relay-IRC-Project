d044e7294ffd455ea6931ceaab6d991478b4e1cc
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-02-05 13:03:09 +0100

    Messages APIs done

---
+++ server/Controllers/messageController.ts
@@ -0,0 +1,38 @@
+import messageModel from "../Models/messageModel";
+
+// createMessage
+
+export const createMessage = async(req : any, res : any) => {
+    const {chatId, senderId, text} = req.body
+
+    const message = new messageModel({
+        chatId, senderId, text
+    })
+
+ try    {
+        const response= await message.save()
+        res.status(200).json(response);
+    }   catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+// getMessages
+
+export const getMessages= async (req : any, res : any) => {
+    const {chatId} = req.params;
+
+    try {
+        const messages = await messageModel.find({chatId})
+        res.status(200).json(messages);
+    }   catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+module.exports = { createMessage, getMessages };
+
+
+
